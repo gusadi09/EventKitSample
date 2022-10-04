cec23231652371d5669211b5ebb464459bba57dc
Agus Adi <gusadi872@gmail.com>
2022-10-04 13:40:07 +0700

    Initial Commit

---
+++ EventKitSample/EventKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventKitSampleApp.swift
+//  EventKitSample
+//
+//  Created by Gus Adi on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct EventKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
